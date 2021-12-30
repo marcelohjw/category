3a28a8f99a41089a7c69cd73fe05a40fc879ca5e
Marcelo Júnior <marcelohjw@gmail.com>
2021-12-30 18:22:45 -0300

    Grid Styling & Some Refactoring

--- data/dummy-data.js
+++ data/dummy-data.js
@@ -2,7 +2,7 @@ import Category from "../models/category";
 
 export const CATEGORIES = [
     new Category('c1', 'Italian', '#f5428d'),
-    new Category('c2', 'Quick & Easy', '#f54242'),
+    new Category('c2', 'Quick & Easy Peasy', '#f54242'),
     new Category('c3', 'Hamburguers', '#f5a442'),
     new Category('c4', 'German', '#f5d142'),
     new Category('c5', 'Light & Lovely', '#368dff'),
