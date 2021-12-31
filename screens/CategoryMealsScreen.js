22409d400b1edf764237fb2fa9b9add723b38904
Marcelo Júnior <marcelohjw@gmail.com>
2021-12-31 09:00:28 -0300

    Rendering a Meals list properly

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { View, Text, FlatList, StyleSheet } from 'react-native';
 
 import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 const CategoryMealsScreen = props => {
     const renderMealItem = itemData => {
         return (
-            <View>
-                <Text>{itemData.item.title}</Text>
-            </View>
+            <MealItem 
+                title={itemData.item.title}
+                image={itemData.item.imageUrl} 
+                onSelect={() => { }}
+                duration={itemData.item.duration}
+                complexity={itemData.item.complexity}
+                affordability={itemData.item.affordability} 
+            />
         );
     };
 
@@ -20,7 +26,12 @@ const CategoryMealsScreen = props => {
 
     return (
         <View style={styles.screen}>
-            <FlatList data={displayedMeals} keyExtractor={(item, index) => item.id} renderItem={renderMealItem}/>
+            <FlatList 
+                data={displayedMeals} 
+                keyExtractor={(item, index) => item.id} 
+                renderItem={renderMealItem} 
+                style={{ width: '100%' }} 
+            />
         </View>
     );
 }
