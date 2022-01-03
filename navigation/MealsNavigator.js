8f6be8783640ae8d68ca6ceba849ec0555e2dcb7
Marcelo Júnior <marcelohjw@gmail.com>
2022-01-03 10:33:54 -0300

    SOLVED THE MF ERROR WITH REACT-NATIVE-REANIMATED FOR THE FAVORITE TABS

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,10 +1,12 @@
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator({
@@ -23,4 +25,11 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-export default createAppContainer(MealsNavigator);
+const MealsFavTabNavigator = createBottomTabNavigator({
+    Meals: {
+        screen: MealsNavigator
+    },
+    Favorites: FavoritesScreen
+});
+
+export default createAppContainer(MealsFavTabNavigator);
