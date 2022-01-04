bd6885bd08cfaada813461f848320e0fdead5631
Marcelo Júnior <marcelohjw@gmail.com>
2022-01-04 09:18:42 -0300

    Adding a stack favorite navigator 1

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -11,6 +11,13 @@ import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoritesScreen from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
+const defaultStackNavOptions = {
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+};
+
 const MealsNavigator = createStackNavigator({
     Categories: CategoriesScreen,
     CategoryMeals: {
@@ -19,12 +26,15 @@ const MealsNavigator = createStackNavigator({
     MealDetail: MealDetailScreen
 },
 {
-    defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
-        },
-        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
-    }
+    defaultNavigationOptions: defaultStackNavOptions
+});
+
+const FavNavigator = createStackNavigator({
+    Favorites: FavoritesScreen,
+    MealDetail: MealDetailScreen
+},
+{
+    defaultNavigationOptions: defaultStackNavOptions
 });
 
 const MealsFavTabNavigator = createBottomTabNavigator({
@@ -37,7 +47,7 @@ const MealsFavTabNavigator = createBottomTabNavigator({
         }
     },
     Favorites: {
-        screen: FavoritesScreen,
+        screen: FavNavigator,
         navigationOptions: {
             tabBarIcon: (tabInfo) => {
                 return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />
