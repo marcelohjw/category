50bf1558ea95c5381c4121e263fac70cea5df24f
Marcelo Júnior <marcelohjw@gmail.com>
2022-01-06 05:58:29 -0300

    Started to work on the filters screen

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Switch } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
 
 const FiltersScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text>The Filters Screen!</Text>
+            <Text style={styles.title}>Available Filters / Restrictions</Text>
+            <View style={styles.filterContainer}>
+                <Text>Gluten-free</Text>
+                <Switch />
+            </View>
         </View>
     );
 }
@@ -27,6 +31,18 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center'
+    },
+    filterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%'
     }
 })
 
